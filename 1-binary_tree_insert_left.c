74696c578b4960c971fc0c0082da8ab82f185490
mefdev <mohamedmeftah2018@gmail.com>
2023-10-31 19:49:34 +0100

    Insert on the left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_insert_left - Insert a new left node
+ *
+ * @parent: the parent node
+ * @value: the values to be inserted
+ *
+ * Return: the new node
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newLeftNode;
+
+	newLeftNode = malloc(sizeof(binary_tree_t));
+	newLeftNode = binary_tree_node(parent, value);
+	if (!parent->left)
+	{
+		parent->left = newLeftNode;
+	}
+	else
+	{
+		parent->left->parent = newLeftNode;
+		newLeftNode->left = parent->left;
+		parent->left = newLeftNode;
+	}
+	return (newLeftNode);
+}
