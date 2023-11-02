25bf34cca5075545d8d5133043ed1d5b7c951647
mefdev <mohamedmeftah2018@gmail.com>
2023-11-02 20:48:28 +0100

    Check for root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -4,22 +4,17 @@
  * binary_tree_is_root - Check for root
  *
  * @node: the tree to check
- * Return: if leaf return 1 otherwise 0
+ * Return: if root return 1 otherwise 0
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	int isRoot;
 
-	isRoot = (node->parent == NULL);
 	if (!node)
 	{
 		return (0);
 	}
-
+	isRoot = (node->parent == NULL);
 	return (isRoot);
-
-
-
-
 }
