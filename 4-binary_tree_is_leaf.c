c8866044ca0f5728cc486b4cb78cef55c8388bd1
mefdev <mohamedmeftah2018@gmail.com>
2023-11-02 20:13:33 +0100

    Is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -17,11 +17,11 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	binary_tree_is_leaf(node->right);
 	if (node->left == NULL && node->right == NULL)
 	{
-		return 1;
+		return (1);
 	}
 	else
 	{
-		return 0;
+		return (0);
 	}
 
 
