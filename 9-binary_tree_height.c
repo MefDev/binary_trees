ba22448a91b3f2fea1b26553c75457f7163b5cad
mefdev <mohamedmeftah2018@gmail.com>
2023-11-02 21:43:57 +0100

    Count the height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - measure the height of a binary tree
+ *
+ * @tree: is a pointer to the root node of the tree to traverse
+ * Return: height or 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (0);
+	}
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+
+	if (left > right)
+	{
+		return (left + 1);
+	}
+	else
+	{
+		return  (right + 1);
+	}
+}
