a8da33753bcfdeea52ee3ad7344106835ecfdf89
mefdev <mohamedmeftah2018@gmail.com>
2023-11-02 19:33:45 +0100

    Insert on the right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,7 +2,7 @@
 
 
 /**
- * binary_tree_insert_left - Insert a new right node
+ * binary_tree_insert_right - Insert a new right node
  *
  * @parent: the parent node
  * @value: the values to be inserted
@@ -13,8 +13,15 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newrightNode;
 
-	newrightNode = malloc(sizeof(binary_tree_t));
+	if (!parent)
+	{
+		return (NULL);
+	}
 	newrightNode = binary_tree_node(parent, value);
+	if (!newrightNode)
+	{
+		return (NULL);
+	}
 	if (!parent->right)
 	{
 		parent->right = newrightNode;
